d51ba37189d9340c210a664f21624dd2e3f8d27a
Luiz Fernando <luizbaizi1243@gmail.com>
2025-02-10 20:44:59 -0300

    Registro de dados

---
+++ home.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site | login/cadastro</title>
+    <style>
+        body{
+            font-family: Arial, Helvetica, sans-serif;
+            background: linear-gradient(to right, rgb(20, 147, 220), rgb(17, 54, 71));
+            text-align: center;
+            color: white;
+        }
+        .box{
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: -50%, -50%;
+            background-color: rgba(0, 0, 0, 0.6)
+            padding: 30px;
+            border-radius: 15px;
+        }    
+        
+    </style>
+</head>
+<body>
+    <h1>Faça o Login ou Registre-se!</h1>
+    <div class="box">
+        <a href="">Login</a>
+        <a href="">Cadastre-se</a>
+    </div>
+</body>
+</html>
